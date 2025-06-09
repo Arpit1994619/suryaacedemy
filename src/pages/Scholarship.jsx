f9bc0431b7dda5cfb7045996a9714bc2b67387ad
arpit kumar <arpitk714@gmail.com>
2025-06-09 14:48:57 +0530

    all

--- src/pages/Scholarship.jsx
+++ src/pages/Scholarship.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
+import image1 from './pagesIMAGES/scholarshipImage/1.png'
+
 import './pageCSS/Scholarship.css'
 
  function Scholarship() {
   return (
-    <div className='scholar'>Scholarship</div>
+    <div className='scholar'>
+        <img src={image1} alt="image1" width='100%' />
+    </div>
   )
 }
 export default Scholarship
